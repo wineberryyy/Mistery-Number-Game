fbbd275e27b94a284e7248af9f17dcbc64fd137e
Heloysa <134120126+wineberryyy@users.noreply.github.com>
2024-08-25 21:41:19 -0300

    Update Mistery_number.py

--- Mistery_number.py
+++ Mistery_number.py
@@ -4,18 +4,15 @@ def mystery_number():
     print("Welcome to the Mystery Number game!")
     print("I'm thinking of a number between 1 and 100. Can you guess it?")
     
-    # Computer chooses a random number
     secret_number = random.randint(1, 100)
     
     attempts = 0
     guessed_correctly = False
     
     while not guessed_correctly:
-        # The player makes a guess
         guess = int(input("Enter your guess: "))
         attempts += 1
-        
-        # Check if the guess is correct
+
         if guess == secret_number:
             guessed_correctly = True
             print(f"Congratulations! You uncovered the mystery number in {attempts} attempts.")
